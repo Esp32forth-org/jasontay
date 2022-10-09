5492f7bffcdbf144e9a546eaab865083272ca454
Jason Tay <86388228+trozodejamon@users.noreply.github.com>
2022-10-09 17:19:12 +0800

    Initial commit of SPI-Userwords
    SPI-Userwords adds definitions for a direct Forth-to-Arduino-C SPI interface layer without any device specifics. You can then call these Arduino SPI API calls to directly talk to SPI devices.

---
+++ userwords-spi.h
@@ -0,0 +1,13 @@
+/* User defined words in here */
+
+/* Added by Jason CJ Tay (shutay@gmail.com) */
+/* For basic SPI support: */
+#include <SPI.h>
+
+#define USER_WORDS \
+  Y(SPIbegin, SPI.begin()) \
+  Y(SPIbeginTransaction, SPI.beginTransaction(SPISettings(n2, n1, n0)); DROPn(3)) \
+  Y(SPItransfer, SET SPI.transfer(n0) ) \
+  Y(SPItransfer16, SET SPI.transfer16(n0) ) \
+  Y(SPIendTransaction, SPI.endTransaction()) \
+  Y(SPIend, SPI.end()) \
