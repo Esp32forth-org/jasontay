0058d75a799da1da01a11468d631d9278e78d899
Jason Tay <86388228+trozodejamon@users.noreply.github.com>
2023-02-09 21:30:37 +0800

    Update userwords.h
    Added Circle and FillCircle words.

--- M5Stack/userwords.h
+++ M5Stack/userwords.h
@@ -32,6 +32,8 @@
   Y(M5Line, M5.Lcd.drawLine(n4, n3, n2, n1, n0); DROPn(5)) \
   Y(M5Rect, M5.Lcd.drawRect(n4, n3, n2, n1, n0); DROPn(5)) \
   Y(M5FillRect, M5.Lcd.fillRect(n4, n3, n2, n1, n0); DROPn(5)) \
+  Y(M5Circle, M5.Lcd.drawCircle(n3, n2, n1, n0); DROPn(4)) \
+  Y(M5FillCircle, M5.Lcd.fillCircle(n3, n2, n1, n0); DROPn(4)) \
   Y(M5Print, M5.Lcd.print(c0); DROP) \
   Y(M5DrawStr, M5.Lcd.drawString(c3, n2, n1, n0); DROPn(4)) \
   Y(M5DrawNum, M5.Lcd.drawNumber(n2, n1, n0); DROPn(3)) \
